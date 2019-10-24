316b7655aa37d4dde47f83631377c2cd78030d3f
Tri Do <trido1903@gmail.com>
2019-10-23 20:58:37 -0700

    Modified the selection sort

--- src/component/SelectionSort/sketch.js
+++ src/component/SelectionSort/sketch.js
@@ -17,8 +17,15 @@ export default function sketch (p) {
     p.draw = function () {
         p.background("#ffffff");
         for (let i = 0; i < rectangles.length; i++) {
-
-            // p.text(rectangles[i].value, i * rectWidth + 6, 390-20);
+            if(rectangles[i].state == -1){
+                p.fill("#78e08f");
+            }else if(rectangles[i].state == 0){
+                p.fill("#ff6b81");
+            }else if(rectangles[i].state == 2){
+                p.fill("#feca57");
+            }else{
+                p.fill("#17c0eb");
+            }          
             p.rect(i * rectWidth, 390 - rectangles[i].value - 40, rectWidth, rectangles[i].value, 8, 8, 0, 0);
         }
     };
@@ -26,25 +33,30 @@ export default function sketch (p) {
     async function selectionSort(){
         for(let i = 0; i < rectangles.length; i++){
             let lowest = i;
-
             for(let j = i + 1; j < rectangles.length; j++){
-
+                rectangles[j].state = 3;
                 if(rectangles[j].value < rectangles[lowest].value){
                     lowest = j;
                 }
+
             }
+            rectangles[i].state = -1;
             if(lowest != i){
+                rectangles[i].state = 0;
+                rectangles[lowest].state = 0;
                 await swap(lowest, i);
+                rectangles[i].state = -1;
+                rectangles[lowest].state = -1;
             }
         }
     }
 
      async function swap(i, j) {
-
-        await sleep(400);
+        await sleep(1000);
         let temp = rectangles[i].value;
         rectangles[i].value = rectangles[j].value;
         rectangles[j].value = temp;
+        
     }
 
     function sleep(time) {
